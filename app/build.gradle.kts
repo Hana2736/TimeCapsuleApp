1d00e91d237f96a6de052305586c2a43630b8dba
Travis Genzer <tlgenzer@gmail.com>
2024-10-26 18:45:32 -0500

    Added create cap

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "lol.hana.timecapsule"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "lol.hana.timecapsule"
-        minSdk = 33
-        targetSdk = 34
+        minSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -29,7 +29,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
@@ -37,4 +36,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    //implementation ("com.google.android.material:material:1.11.0-alpha01")
+//implementation(libs.material3)
+    //implementation("androidx.compose.material3:material3-window-size-class:1.3.0")
+   // implementation("androidx.compose.material3:material3-adaptive-navigation-suite:1.4.0-alpha02")
 }
--- app/src/main/java/lol/hana/timecapsule/MainActivity.java
+++ app/src/main/java/lol/hana/timecapsule/MainActivity.java
@@ -3,12 +3,15 @@ package lol.hana.timecapsule;
 import android.os.Bundle;
 
 import android.view.View;
+import android.view.Window;
 import android.widget.LinearLayout;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowCompat;
 import androidx.core.view.WindowInsetsCompat;
+import com.google.android.material.color.DynamicColors;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,12 +21,19 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.statusBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
         LinearLayout layout = findViewById(R.id.mainPageScrollLayout);
-        layout.addView(View.inflate(this, R.layout.caps_view,null));
+        for(int i =0; i < 50; i++)
+            layout.addView(View.inflate(this, R.layout.caps_view,null));
+
+
+
+        DynamicColors.applyToActivitiesIfAvailable(getApplication());
+        //getActionBar().hide();
+        //WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
     }
 }
